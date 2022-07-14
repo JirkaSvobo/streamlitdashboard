653d9f2175fbef00161d10d74fbe51265bfd1826
JirkaSvobo <79328364+JirkaSvobo@users.noreply.github.com>
2022-07-14 19:30:24 +0200

    dodelani mapy
    misto testu je tam mapa

--- main_app.py
+++ main_app.py
@@ -1,14 +1,33 @@
 import streamlit as st
 
+from sqlalchemy import create_engine
+import pandas as pd
+import pymysql
+
+import plotly.graph_objects as go
+import plotly.express as px
+
+##########
+# data
+##########
+engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
+df_bikes = pd.read_sql(sql='select * from edinburgh_bikes', con=engine)
+
+##########
+# vizualizace
+##########
 
 st.title('Moje prvni appka')
 
 st.write('Toto je moje prvni aplikace, kterou udelam. Dalsi radky budou super COOL!')
 
-page = st.sidebar.radio('Select page', ['Test', 'Tomson'])
+page = st.sidebar.radio('Select page', ['Mapa', 'Thomson'])
 
-if page == 'Test':
-    st.write('Toto je moje prvni aplikace, kterou udelam. Dalsi radky budou super COOL!')
+if page == 'Mapa':
+    st.write('Moapa pouzivani sdilenych kol v Edinburgu')
+    fig = px.scatter_mapbox(df_temp,lat='start_station_latitude', lon='start_station_longitude')
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.show()
 
-if page == 'Tomson':
+if page == 'Thomson':
     st.write('Tomson sampling')
