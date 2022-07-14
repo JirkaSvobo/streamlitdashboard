ccc1796ef477636720e72a1f9e2823c2572cc3cb
JirkaSvobo <79328364+JirkaSvobo@users.noreply.github.com>
2022-07-14 19:16:49 +0200

    pridani radio buttonu

--- main_app.py
+++ main_app.py
@@ -4,3 +4,11 @@ import streamlit as st
 st.title('Moje prvni appka')
 
 st.write('Toto je moje prvni aplikace, kterou udelam. Dalsi radky budou super COOL!')
+
+page = st.sidebar.radio('Select page', ['Test', 'Tomson'])
+
+if page == 'Test':
+    st.write('Toto je moje prvni aplikace, kterou udelam. Dalsi radky budou super COOL!')
+
+if page == 'Tomson':
+    st.write('Tomson sampling')
