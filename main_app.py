4a166e205a36b7bc53135794a211cb99c15931d0
JirkaSvobo <79328364+JirkaSvobo@users.noreply.github.com>
2022-07-14 20:27:10 +0200

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -12,20 +12,20 @@ import plotly.express as px
 ##########
 engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
 query_morning = """SELECT
-             start_station_latitude as lat,
-             start_station_longitude as lon
-             FROM edinburgh_bikes
-             WHERE hour(started_at) BETWEEN 6 AND 9
-             LIMIT 100000
- """
-
- query_afternoon = """SELECT
-              start_station_latitude as lat,
-              start_station_longitude as lon
-              FROM edinburgh_bikes
-              WHERE hour(started_at) BETWEEN 15 AND 19
-              LIMIT 100000
-  """
+                    start_station_latitude as lat,
+                    start_station_longitude as lon
+                FROM edinburgh_bikes
+                WHERE hour(started_at) BETWEEN 6 AND 9
+                LIMIT 100000
+                """
+query_afternoon = """SELECT
+                      start_station_latitude as lat,
+                      start_station_longitude as lon
+                  FROM edinburgh_bikes
+                  WHERE hour(started_at)
+                  BETWEEN 15 AND 19
+                  LIMIT 100000
+"""
 df_bikes_morning = pd.read_sql(sql=query_morning, con=engine)
 df_bikes_afternoon = pd.read_sql(sql=query_afternoon, con=engine)
 
